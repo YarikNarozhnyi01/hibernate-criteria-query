0fcba1ce348e34aa6c3fb8c7c82604f641caaca9
Yaroslav Narozhnyi <gnredit9@gmail.com>
2025-07-30 20:31:33 +0300

    fourth

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,10 +1,10 @@
 package ma.hibernate.dao;
 
-import java.util.Arrays;
 import jakarta.persistence.criteria.CriteriaBuilder;
 import jakarta.persistence.criteria.CriteriaQuery;
 import jakarta.persistence.criteria.Predicate;
 import jakarta.persistence.criteria.Root;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
